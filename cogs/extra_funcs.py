dfa5f485cd0abcfab8a932a57e5121979952ebf8
Matt Monte <mcmonte@protonmail.com>
2020-03-22 01:58:07 -0400

    Cleaned up existing documentation, and added some where needed.

--- cogs/extra_funcs.py
+++ cogs/extra_funcs.py
@@ -6,10 +6,7 @@ import random
 
 
 def duplicate_count(polls, poll_name):
-    """A function which accepts a list of dictionaries containing poll data and poll_name which is 
-    a discord message object. it then returns the number of times that the poll name appears in 
-    the list. 
-    """
+    """Returns number of duplicate poll names in a list of dictionaries."""
     counter = 0
     for poll_dict in polls:
         if poll_dict['name'] == poll_name.content:
@@ -27,28 +24,29 @@ def duplicate_count(polls, poll_name):
 
 
 def dump_poll_data(polls):
-    """Accepts a list of dictionaries each representing a poll and dumps them to a json file.
-    """
+    """Writes poll data from a list of dictionaries to a json file."""
     with open('./cogs/polls.json', 'w', encoding='utf-8') as poll_file:
         json.dump(polls, poll_file, indent=2)
 
 
 def load_poll_data():
-    """Loads poll data in as a list of dictionaries from a json file
-    """
+    """Reads poll data from a json file into a list of dictionaries."""
     polls = []
     with open('./cogs/polls.json', 'r', encoding='utf-8') as poll_file:
         polls = json.load(poll_file)
     return polls
 
 def generate_hex_int():
-    """returns a randomly generated hexadecimal integer"""
+    """Returns a randomly generated hexadecimal integer."""
     random_num = random.randint(0, 16777215)
     hex_num = hex(random_num)
     return int(hex_num, 16)
 
 def get_next_index(dict_list):
-    """
+    """Returns the next index for a list of dictionaries.
+
+    Keyword Arguments:
+    dict_list -- a list of dictionaries
     """
     new_index = ""
     try:
@@ -67,7 +65,7 @@ def format_date(date_dict):
     """Returns a date formatted in mm-dd-yyyy.
 
     Keyword Arguments:
-    date_dict -- dict containing data information
+    date_dict -- dict containing date information
     """
     formatted_date = 'n/a'
     if date_dict['month'] != None:
@@ -79,9 +77,10 @@ def format_date(date_dict):
     return formatted_date
 
 def list_to_str(genres):
-    """Accepts a list of strings and converts it to a string with each element separated by a comma
+    """Returns a string containing each list element seperated by a comma.
+
+    Keyword Arguments:
+    genres -- the list to be formatted
     """
     genres = ', '.join(genre for genre in genres)
     return genres
-
-
